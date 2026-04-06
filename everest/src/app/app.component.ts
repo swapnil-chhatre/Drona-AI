fa932537d029e1e48f269d7182e78be10d239351
Ruben Thomas <rubenthomas812@gmail.com>
2026-04-06 15:06:17 +1000

    chore: Cleaned up app component to use only for routing

--- everest/src/app/app.component.ts
+++ everest/src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DiscoverComponent } from '../discover/discover.component';
-import { SidebarComponent } from '../common/sidebar/sidebar.component';
-import { SidebarNavItem } from '../common/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DiscoverComponent, SidebarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  protected readonly sidebarItems: SidebarNavItem[] = [
-    { label: 'Dashboard', icon: 'dashboard', route: '/', active: true },
-    { label: 'My Study Plans', icon: 'menu_book' },
-    { label: 'Uploaded Docs', icon: 'upload_file' },
-    { label: 'Settings', icon: 'settings' },
-  ];
-}
+export class AppComponent { }
+
